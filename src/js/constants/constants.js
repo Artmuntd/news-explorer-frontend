ffedc7ad812405023823b00b805e573b25412386
Artmuntd <69253476+Artmuntd@users.noreply.github.com>
2021-02-22 18:34:09 +0300

    fix problem

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,7 +1,7 @@
 const constants = {
   API_KEY: "6911322d57a545738f4f119dd1aaca11",
   NEWS_URL: "https://nomoreparties.co/news/v2/everything",
-  MAIN_URL: "http://www.artmuntd.students.nomoreparties.xyz/api",
+  MAIN_URL: "http://www.artmuntd.students.nomoreparties.xyz",
   TOKEN: localStorage.getItem("token"),
   PAGE: document,
   TEMPLATE: document.getElementById("card-template"),
