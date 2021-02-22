12d2e41d470d1c51481277c13ad1b14aee9cecfe
Artmuntd <69253476+Artmuntd@users.noreply.github.com>
2021-02-22 20:41:13 +0300

    fix problem

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,11 +1,11 @@
 const constants = {
   API_KEY: "6911322d57a545738f4f119dd1aaca11",
   NEWS_URL: "https://nomoreparties.co/news/v2/everything",
-  MAIN_URL: "http://www.artmuntd.students.nomoreparties.xyz",
+  MAIN_URL: "https://www.api.artmuntd.students.nomoreparties.space",
   TOKEN: localStorage.getItem("token"),
   PAGE: document,
   TEMPLATE: document.getElementById("card-template"),
 };
 
 export default constants;
-//https://nomoreparties.co/news/v2/top-headlines?country=us&apiKey=[ваш_ключ]  при деплое надо будет поставить.
+//https://nomoreparties.co/news/v2/top-headlines?country=us&apiKey=[ваш_ключ]  при деплое надо будет поставить
